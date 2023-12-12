ef99f5c67681e2ed4d4051a07f7fef4586f27185
Gökçe Lal ARSLAN <116730836+GokceLal@users.noreply.github.com>
2023-12-12 09:07:39 +0300

    Add files via upload

---
+++ src/main/java/org/example/Runner.java
@@ -0,0 +1,51 @@
+//package org.example.FileOdev;
+
+import java.io.*;
+import java.nio.Buffer;
+
+
+//public class Runner {
+
+
+
+
+   //HASTANEYE GİTMEM GEREKTİĞİ İÇİN ÖDEVİ YAPAMADIM
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//static String path = "E://Users/gokcelal/Documents//deneme.txt";
+
+
+
+
+
+   /** public static void main(String[] args) {
+        BufferedReader reader;
+        BufferedWriter writer = new BufferedWriter();
+        try{
+            try {
+                FileWriter fileWriter = new FileWriter(path);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
+
+    }
+}
+
+    */
